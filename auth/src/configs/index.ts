24fb851b873cb2bd6ea7b595af81b04f9994ced1
Esmail Elmoussel <elmoussel12@gmail.com>
2022-09-17 23:05:37 +0200

    feat(client): dockerize client app

--- auth/src/configs/index.ts
+++ auth/src/configs/index.ts
@@ -2,10 +2,6 @@ if (!process.env.JWT_SECRET) {
   throw new Error('Missing env variable "JWT_SECRET"');
 }
 
-if (!process.env.NODE_ENV) {
-  throw new Error('Missing env variable "NODE_ENV"');
-}
-
 export const configs = {
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
---
+++ client/pages/apple.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Apple: NextPage = () => {
+  return <h1>Apple</h1>;
+};
+
+export default Apple;
