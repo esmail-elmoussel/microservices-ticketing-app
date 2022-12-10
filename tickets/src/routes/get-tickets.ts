2ecef47a3d2ec4505600f00bdb2a006e587ba0ec
Esmail Elmoussel <elmoussel12@gmail.com>
2022-12-10 20:54:22 +0200

    feat(tickets): filter reserved tickets

--- tickets/src/routes/get-tickets.ts
+++ tickets/src/routes/get-tickets.ts
@@ -4,7 +4,7 @@ import { Ticket } from "../models/ticket.model";
 const router = Router();
 
 router.get("/api/tickets", async (req, res) => {
-  const tickets = await Ticket.find();
+  const tickets = await Ticket.find({ orderId: undefined });
 
   return res.json(tickets);
 });
